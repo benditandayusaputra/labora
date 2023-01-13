9d6d985089cb2d83d0f877fadbb9dc56c2d63ca1
Ibnu Shevayanto <ibnushevayanto@gmail.com>
2023-01-13 22:58:39 +0700

    progress 13 januari 2022

--- mobile/src/screens/FormTournament.js
+++ mobile/src/screens/FormTournament.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {TouchableOpacity, ScrollView, StyleSheet} from 'react-native';
+import {TouchableOpacity, ScrollView, StyleSheet, Alert} from 'react-native';
 import {View, Text, Button, Picker, Incubator} from 'react-native-ui-lib';
 import tailwind from 'twrnc';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -13,13 +13,43 @@ const options = [
 ];
 export default function ({navigation}) {
   const [clubModel, setClubModel] = useState(null);
-  const [itemsInputanNama, setItemsInputanNama] = useState([
-    'Ibnu',
-    'Bendi',
-    'Fauzi',
-    'Teguh',
-    'Mamat',
-  ]);
+  const [itemsInputanNama, setItemsInputanNama] = useState([]);
+  const [modelTextNama, setModelTextNama] = useState(null);
+  const [modelNomorTelepon, setModelNomorTelepon] = useState(null);
+
+  const addPersonHandler = () => {
+    if (itemsInputanNama.length < 5) {
+      setItemsInputanNama(prevState => prevState.concat(modelTextNama));
+      setModelTextNama(null);
+    } else {
+      Alert.alert(
+        'Gagal',
+        'Jumlah nama yang didaftarkan tidak boleh lebih dari 5',
+        ['Oke'],
+      );
+    }
+  };
+  const deleteItemHandler = nama => {
+    Alert.alert('Konfirmasi', `Apakah anda yakin ingin menghapus ${nama}?`, [
+      {text: 'Batal', style: 'cancel'},
+      {
+        text: 'Yakin',
+        style: 'destructive',
+        onPress: () => {
+          setItemsInputanNama(prevState =>
+            prevState.filter(item => item !== nama),
+          );
+        },
+      },
+    ]);
+  };
+  const prosesDaftarHandler = () => {
+    if (modelNomorTelepon && clubModel && itemsInputanNama.length) {
+      console.log('proses daftar');
+    } else {
+      Alert.alert('Gagal', 'Mohon cek kembali form anda', ['Oke']);
+    }
+  };
 
   return (
     <View flex style={tailwind`p-4`}>
@@ -70,11 +100,13 @@ export default function ({navigation}) {
           <View row centerV style={tailwind`mb-2`}>
             <View flexG>
               <TextField
+                value={modelTextNama}
+                onChangeText={value => setModelTextNama(value)}
                 placeholder="Input Nama"
                 style={styles.underlineField}
               />
             </View>
-            <TouchableOpacity style={tailwind`ml-2`}>
+            <TouchableOpacity style={tailwind`ml-2`} onPress={addPersonHandler}>
               <Ionicons name="add-circle" size={32} color={'#293241'} />
             </TouchableOpacity>
           </View>
@@ -88,7 +120,9 @@ export default function ({navigation}) {
                 row
                 centerV>
                 <Text style={{fontFamily: 'SFNSDisplay-Bold'}}>{item}</Text>
-                <TouchableOpacity style={tailwind`ml-2`}>
+                <TouchableOpacity
+                  style={tailwind`ml-2`}
+                  onPress={() => deleteItemHandler(item)}>
                   <Ionicons name="close-circle-sharp" color={'red'} size={24} />
                 </TouchableOpacity>
               </View>
@@ -100,7 +134,9 @@ export default function ({navigation}) {
             Nomor Telepon
           </Text>
           <TextField
+            value={modelNomorTelepon}
             keyboardType="number-pad"
+            onChangeText={value => setModelNomorTelepon(value)}
             placeholder="Input Nomor Telepon"
             style={styles.underlineField}
           />
@@ -109,6 +145,7 @@ export default function ({navigation}) {
       </ScrollView>
       <Button
         label="Daftar"
+        onPress={prosesDaftarHandler}
         style={tailwind`mt-4`}
         size={Button.sizes.large}
         backgroundColor="#293241"
